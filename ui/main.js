9acb1c7538250ef5b31547435f7d9b9069281cf0
Sweta-samaddar <15ce116@charusat.edu.in>
2017-02-12 01:21:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,16 @@ console.log('Loaded!');
 
 var txt=document.getElementById('main');
 txt.innerHTML="new alue ";
+
+var img=document.getElementById('img');
+
+var move=0;
+moveleft()
+{
+    move=move+10;
+    img.style.marginLeft=move+'px';
+}
+img.OnClick=function()
+{
+  var int=setInterval(moveLeft,100);  
+};
